f3d1fa4f95def6f879735a798e5c4570d669c7a2
Bikram <sbikrm@gmail.com>
2020-11-14 13:23:02 +0800

    "Two Sum Commit"

--- src/Array_Problems/TwoSum.java
+++ src/Array_Problems/TwoSum.java
@@ -1,9 +1,22 @@
 package Array_Problems;
 
 public class TwoSum {
-
+    int[] nums = {2,7,11,15}; // output[0, 1]
+    int target = 9;
     TwoSum(){
+        System.out.println(twoSum(nums, target));
+    }
 
+    public int[] twoSum(int[] nums, int target){
+
+        for(int i=0; i< nums.length; i++){
+            for(int j=i+1; j< nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
     }
     public static void main(String[] args){
         new TwoSum();
