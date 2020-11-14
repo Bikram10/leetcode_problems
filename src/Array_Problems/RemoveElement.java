55c89a3359ad4ecc1e0cb6d2d1c26939fd4ae2c3
Bikram <sbikrm@gmail.com>
2020-11-14 14:02:15 +0800

    "Remove Element Commit"

--- src/Array_Problems/RemoveElement.java
+++ src/Array_Problems/RemoveElement.java
@@ -9,12 +9,13 @@ public class RemoveElement {
 
     public int removeElement(int[] nums, int val){
         int i=0;
-        for(int j=1; j< nums.length; j++){
-            if(nums[j] !=val)
-                i++;
+        for(int j=0; j< nums.length; j++){
+            if(nums[j] !=val) {
                 nums[i] = nums[j];
+                i++;
+            }
         }
-        return i+1;
+        return i;
     }
 
     public static void main(String[] args){
